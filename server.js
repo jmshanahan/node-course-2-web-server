2c3f0e744c487b0d44693e4fc5ad84ffc75586c2
jmshanahan <comeraghsolutions@gmail.com>
2017-07-10 20:40:31 +0100

    Put through small change just to check github login

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ hbs.registerHelper('getCurrentYear', () => {
 hbs.registerHelper('screamIt', (text) => {
     return text.toUpperCase();
 });
+
 app.get('/', (req, res) => {
 
     res.render('home', {
